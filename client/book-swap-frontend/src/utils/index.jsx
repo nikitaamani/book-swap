f0136f289c57e4e5589e90c7ddca1412ffb54b73
nikitaamani <nikita.bosire@student.moringaschool.com>
2025-01-30 15:21:06 +0300

    created utils

---
+++ client/book-swap-frontend/src/utils/index.jsx
@@ -0,0 +1,15 @@
+import {
+  signupValidationSchema,
+  loginValidationSchema,
+} from "./validationSchemas";
+import { showErrorToast, showInfoToast, showSuccessToast } from "./toastUtils";
+import { trimText } from "./trimText";
+
+export {
+  signupValidationSchema,
+  loginValidationSchema,
+  showErrorToast,
+  showInfoToast,
+  showSuccessToast,
+  trimText,
+};
